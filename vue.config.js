b7fca8f1e041b9e8bcbd65d06f8981200d8d41dc
dyr <1538253901@qq.com>
2021-02-07 10:13:14 +0800

    修改代码自动刷新

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
+const merge = require('webpack-merge');
+console.log('当前打包环境', process.env.VUE_APP_SERVER_ENV);
+
 module.exports = {
+  publicPath: './',
+  lintOnSave: true,
+  devServer: {
+    host: '',
+    disableHostCheck: true,
+    port: 80
+  },
   configureWebpack: {
     resolve: {
       alias: {
@@ -8,10 +18,16 @@ module.exports = {
         'views': '@/views',
       }
     },
-    devServer: {
-      open: true,
-      port: 8080,
-    },
-    lintOnSave: false
-  }
-}
+  },
+  // configureWebpack: config => {
+  //   const baseConf = require('./build/webpack.base.conf')(config);
+  //   if (process.env.NODE_ENV === 'production') {
+  //     // 生产环境配置
+  //     return merge(baseConf, require('./build/webpack.prod.conf')(config));
+  //   } else {
+  //     // 开发环境配置
+  //     return merge(baseConf, require('./build/webpack.dev.conf')(config));
+  //   }
+  // }
+};
+
